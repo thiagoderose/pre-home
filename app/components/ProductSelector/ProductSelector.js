bb2c17cb9b57bd9aca5332ac65a5c8490c256536
odelucca <delucca@pm.me>
2020-08-13 12:27:34 -0300

    :construction: WIP

--- app/components/ProductSelector/ProductSelector.js
+++ app/components/ProductSelector/ProductSelector.js
@@ -1,58 +1,66 @@
 import React from 'react';
+import Link from 'next/link';
 import Grid from '@livipdev/core/Grid';
 import ContainerWithBackground from '@livipdev/core/ContainerWithBackground';
 import Box from '@livipdev/core/Box';
 import Logotype from '@livipdev/core/Logotype';
 import Typography from '@livipdev/core/Typography';
+import config from '@livipdev/config';
 
 import messages from './messages';
 
 const ProductSelector = props => (
   <Grid item className={props.classes.root}>
-    <Grid item className={props.classes.product}>
-      <ContainerWithBackground
-        background='images/tickets-background.jpg'
-        customClass={props.classes.tickets}
-      >
-        <Box className={props.classes.inside}>
-          <Logotype variant='tickets' />
-          <Box className={props.classes.hidden}>
-            <Typography message={messages.tickets.description} />
-            <Typography message={messages.learnMore} />
+    <Link href={config.urls.tickets}>
+      <Grid item className={props.classes.product}>
+        <ContainerWithBackground
+          background='images/tickets-background.jpg'
+          customClass={props.classes.tickets}
+        >
+          <Box className={props.classes.inside}>
+            <Logotype variant='tickets' />
+            <Box className={props.classes.hidden}>
+              <Typography message={messages.tickets.description} />
+              <Typography message={messages.learnMore} />
+            </Box>
           </Box>
-        </Box>
-      </ContainerWithBackground>
-    </Grid>
+        </ContainerWithBackground>
+      </Grid>
+    </Link>
 
-    <Grid item className={props.classes.product}>
-      <ContainerWithBackground
-        background='images/travel-background.jpg'
-        customClass={props.classes.travel}
-      >
-        <Box className={props.classes.inside}>
-          <Logotype variant='travel' />
-          <Box className={props.classes.hidden}>
-            <Typography message={messages.tickets.description} />
-            <Typography message={messages.learnMore} />
+    <Link href={config.urls.incentive}>
+      <Grid item className={props.classes.product}>
+        <ContainerWithBackground
+          background='images/travel-background.jpg'
+          customClass={props.classes.travel}
+        >
+          <Box className={props.classes.inside}>
+            <Logotype variant='travel' />
+            <Box className={props.classes.hidden}>
+              <Typography message={messages.tickets.description} />
+              <Typography message={messages.learnMore} />
+            </Box>
           </Box>
-        </Box>
-      </ContainerWithBackground>
-    </Grid>
+        </ContainerWithBackground>
+      </Grid>
+    </Link>
 
-    <Grid item className={props.classes.product}>
-      <ContainerWithBackground
-        background='images/one-background.jpg'
-        customClass={props.classes.one}
-      >
-        <Box className={props.classes.inside}>
-          <Logotype variant='one' />
-          <Box className={props.classes.hidden}>
-            <Typography message={messages.tickets.description} />
-            <Typography message={messages.learnMore} />
+    <Link href={config.urls.one}>
+      <Grid item className={props.classes.product}>
+        <ContainerWithBackground
+          background='images/one-background.jpg'
+          customClass={props.classes.one}
+        >
+          <Box className={props.classes.inside}>
+            <Logotype variant='one' />
+            <Box className={props.classes.hidden}>
+              <Typography message={messages.tickets.description} />
+              <Typography message={messages.learnMore} />
+            </Box>
           </Box>
-        </Box>
-      </ContainerWithBackground>
-    </Grid>
+        </ContainerWithBackground>
+      </Grid>
+    </Link>
   </Grid>
 );
 
